582fa6309d8e247d206f9cedc022893209caf604
oks110 <seokko12@naver.com>
2025-02-20 18:26:20 +0900

    commit

---
+++ src/components/DetailProducts/DetailThumb.jsx
@@ -0,0 +1,21 @@
+export default function DetailThumb() {
+    return (
+        <div class="preview-thumb" id="godImgThumb">
+            <div class="thumb-item active" data="/goods/ORBR/24/10/14/GPDN24101477500_0_THNAIL_ORGINL_20241023161548815.jpg">
+                <button type="button">
+                    <img src="https://img.ssfshop.com/cmd/RB_100x133/src/https://img.ssfshop.com/goods/ORBR/24/10/14/GPDN24101477500_0_THNAIL_ORGINL_20241023161548815.jpg" onerror="javascript:this.src='/v3/images/common/noImg_477.gif'" alt="" />
+                </button>
+            </div>
+            <div class="thumb-item" data="/goods/ORBR/24/10/14/GPDN24101477500_1_THNAIL_ORGINL_20241023161548815.jpg">
+                <button type="button">
+                    <img src="https://img.ssfshop.com/cmd/RB_100x133/src/https://img.ssfshop.com/goods/ORBR/24/10/14/GPDN24101477500_1_THNAIL_ORGINL_20241023161548815.jpg" onerror="javascript:this.src='/v3/images/common/noImg_477.gif'" alt="" />
+                </button>
+            </div>
+            <div class="thumb-item" data="/goods/ORBR/24/10/14/GPDN24101477500_2_THNAIL_ORGINL_20241023161548815.jpg">
+                <button type="button">
+                    <img src="https://img.ssfshop.com/cmd/RB_100x133/src/https://img.ssfshop.com/goods/ORBR/24/10/14/GPDN24101477500_2_THNAIL_ORGINL_20241023161548815.jpg" onerror="javascript:this.src='/v3/images/common/noImg_477.gif'" alt="" />
+                </button>
+            </div>
+        </div>
+    );
+};
