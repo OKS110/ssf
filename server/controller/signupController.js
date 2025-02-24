1024a5290e9efc1b8ee6cd7f0fbafb78f1e93387
oks110 <seokko12@naver.com>
2025-02-24 11:29:21 +0900

    commit

--- server/controller/signupController.js
+++ server/controller/signupController.js
@@ -9,17 +9,22 @@ export const getId = async (req,res) => {
 
 // 회원 등록
 export const registCustomer = async(req,res) => {
-    console.log('회원가입',req.body);
+    console.log(req.body.data.phone);
+    
+    let ph = req.body.data.phone.substring(0,3)
+        .concat('-',req.body.data.phone.substring(3,7),'-',req.body.data.phone.substring(7,11));
+            
 
     const data = {
-        'id':req.body.id,
-        'pwd':req.body.pwd,
-        'username':req.body.username,
-        'phone':req.body.phone,
-        'address':req.body.address,
-        'email': req.body.email.concat('@',req.body.emailDomain) 
+        'id':req.body.data.id,
+        'pwd':req.body.data.pwd,
+        'username':req.body.data.username,
+        'phone':ph,
+        'address':req.body.adata.address.concat(' ',req.body.data.addressDetail),
+        'email': req.body.data.email.concat('@',req.body.data.emailDomain) 
     }
-    
+       
+
     const result = await repository.registCustomer(data);
     res.json(result);
     res.end();
