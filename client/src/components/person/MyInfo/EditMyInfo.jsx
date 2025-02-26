3f2d5b49ae851fcb54b5e6368cad3f65fc9d8565
haon <haoony@naver.com>
2025-02-26 14:08:46 +0900

    14:08

--- client/src/components/person/MyInfo/EditMyInfo.jsx
+++ client/src/components/person/MyInfo/EditMyInfo.jsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React,{useState, useEffect, useRef} from 'react';
 import { SlArrowRight } from "react-icons/sl";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import PersonUIform from '../PersonUIform.jsx';
+import axios from 'axios';
 
 export default function EditMyInfo() {
+    const [data, setData ] = useState([]);
+    const [pw, setPw] = useState('');
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+        const id = localStorage.getItem('user_id');
+        axios.post('http://localhost:9000/mypage/myinfo',{'id':id})
+            .then(res => 
+            setData(res.data)
+            )
+            .catch(error => console.log(error)
+            );
+    },[]);
+
+    const handlePwd = (e) => {
+        // console.log(e.target.value);
+        setPw(...pw, e.target.value);
+    }
+    const checkPwd = () => {
+        if(data.password === pw){
+            navigate('/person/editMemberInfo/updateInfo');
+        } else {
+            alert('비밀번호가 일치하지 않습니다');
+        }
+    }
+
     return (
         <div className="mypage-box">
         <div className="mypage-top-menu">
@@ -21,11 +48,11 @@ export default function EditMyInfo() {
                     <h5 className='mypage-myinfo-pwdCheck'>개인정보보호를 위해 비밀번호를 입력해 주세요.</h5>
                     <div className='mypage-myinfo-pwdCheck-box'>
                         <label htmlFor="">비밀번호</label>                        
-                        <input type="password" 
+                        <input type="password" onChange={handlePwd} 
                             placeholder='비밀번호를 입력해주세요'/>
                     </div>
                     <div className='mypage-myinfo-pwdCheckBtn'>
-                        <Link to='/person/editMemberInfo/updateInfo'><button>확인</button></Link>
+                        <button onClick={checkPwd}>확인</button>
                     </div>
             </article>
         </div>
