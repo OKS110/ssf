ae8ba2ba74703e1b9b9dd96c0517daa161ef5777
oks110 <seokko12@naver.com>
2025-02-28 17:56:18 +0900

    oks 상세페이지 아래 하단 4개 탭

--- client/src/components/DetailProducts/tabsData/Recommend.jsx
+++ client/src/components/DetailProducts/tabsData/Recommend.jsx
@@ -1,4 +1,19 @@
+import { useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useProduct } from '../../../hooks/useProduct';
+import { ProductContext } from '../../../context/ProductContext';
+
+
 export default function Recommend(){
+    const { getPidItem } = useProduct();
+    const { pid} = useParams();
+    const { pidItem, productList } = useContext(ProductContext);
+
+    // 상품 데이터 가져오기 (pid 변경 시마다 실행)
+    useEffect(() => {
+        getPidItem(pid);
+    }, []);
+
     return (
         <div style={{backgroundColor:"lightblue", height:"2000px"}}>
             추천
