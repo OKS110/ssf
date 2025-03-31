fd29c6863bcbcf5ba6f14234d27c21bbfa4bb20a
oks110 <seokko12@naver.com>
2025-03-31 12:28:33 +0900

    commit

--- server/repository/db.js
+++ server/repository/db.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
     port: 3306,
     user: 'root',
     password: 'mysql1234',
-    database: 'shopping_mall'
+    database: 'shopping_mall' //ssf_db
 });
 
 export const db = pool.promise();
